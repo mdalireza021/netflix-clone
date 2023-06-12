5ae0cffd4671937947b4d9029d43865f5ecf80f6
Muhammad Ali <muhammad.ali@brainstation-23.com>
2023-06-12 19:49:39 +0600

    update movies

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -42,7 +42,7 @@ class HomeViewController: UIViewController {
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView;
         
-        //getTrandingMovies();
+        
         //getTrandingTvs();
     }
     
