6d586e95f5d77d1c0d30647b08369ee19caaf67d
Muhammad Ali <muhammad.ali@brainstation-23.com>
2023-06-13 11:45:49 +0600

    remove cross button

--- Netflix Clone/Views/HeroHeaderUIView.swift
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -9,26 +9,6 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
 
-    
-    private let removeButton: UIButton = {
-       
-        let button = UIButton()
-        
-        let configuration = UIImage.SymbolConfiguration(pointSize: 20, weight: .medium);
-            let removeIcon = UIImage(systemName: "xmark.circle.fill", withConfiguration: configuration);
-        
-            button.setImage(removeIcon, for: .normal)
-            button.tintColor = .white
-            button.backgroundColor = .clear
-            button.layer.cornerRadius = button.bounds.width / 2
-            button.layer.masksToBounds = true
-            button.layer.borderColor = UIColor.white.cgColor
-            button.layer.borderWidth = 0
-            button.translatesAutoresizingMaskIntoConstraints = false
-
-            return button
-    }()
-    
     private let downloadButton: UIButton = {
        
         let button = UIButton();
@@ -37,7 +17,6 @@ class HeroHeaderUIView: UIView {
         button.layer.borderWidth = 1;
         button.translatesAutoresizingMaskIntoConstraints = false;
         button.layer.cornerRadius = 5;
-        
         return button;
     }()
     
@@ -49,7 +28,6 @@ class HeroHeaderUIView: UIView {
         button.layer.borderWidth = 1;
         button.translatesAutoresizingMaskIntoConstraints = false;
         button.layer.cornerRadius = 5;
-        
         return button;
         
     }()
@@ -57,11 +35,9 @@ class HeroHeaderUIView: UIView {
     private let heroImageView: UIImageView = {
         
         let imageView = UIImageView()
-        
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds  = true
         imageView.image = UIImage(named: "splash")
-        
         return imageView
     }()
     
@@ -81,56 +57,35 @@ class HeroHeaderUIView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         addSubview(heroImageView)
         addGradient()
         addSubview(playButton)
         addSubview(downloadButton)
-        addSubview(removeButton)
-        
         applyConstraints()
     }
     
-    
     private func applyConstraints() {
         
-        
-        let removeButtonConstraints = [
-        
-            removeButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
-            removeButton.topAnchor.constraint(equalTo: topAnchor, constant: 5),
-            removeButton.widthAnchor.constraint(equalToConstant: 30)
-        ]
-        
         let playButtonConstraints = [
-        
             playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
             playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
             playButton.widthAnchor.constraint(equalToConstant: 120)
         ]
         
         let DownloadButtonConstraints = [
-        
-        
             downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50),
             downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50),
             downloadButton.widthAnchor.constraint(equalToConstant: 120)
-            
         ]
         
         NSLayoutConstraint.activate(playButtonConstraints)
         NSLayoutConstraint.activate(DownloadButtonConstraints)
-        
-        NSLayoutConstraint.activate(removeButtonConstraints)
-        
-        
     }
+    
     override func layoutSubviews() {
         super.layoutSubviews();
         
         heroImageView.frame = bounds;
-        
-        
     }
     
     required init?(coder: NSCoder) {
