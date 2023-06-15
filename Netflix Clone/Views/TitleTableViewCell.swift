e092dac20c7c269d4a01b1526a948b3da5ae24d7
Muhammad Ali <muhammad.ali@brainstation-23.com>
2023-06-15 16:00:36 +0600

    update applyConstraints method

--- Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -51,7 +51,7 @@ class TitleTableViewCell: UITableViewCell {
     private func applyConstraints() {
         let titlesPosterUIImageViewConstraints = [
             titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
             titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
         ]
