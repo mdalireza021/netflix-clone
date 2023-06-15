b7e70553661b0e43925bf00abcbfa69953186e33
Muhammad Ali <muhammad.ali@brainstation-23.com>
2023-06-15 18:02:10 +0600

    remove unnecessary code

--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -8,23 +8,16 @@
 import UIKit
 
 class CollectionViewTableViewCell: UITableViewCell {
-
     static let identifier = "CollectionViewTableViewCell";
-    
     private var titles: [Title] = [Title] ();
     
     private let collectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
-        
         layout.itemSize = CGSize(width: 140, height: 200)
         layout.scrollDirection  = .horizontal
-        
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
-        
         collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.identifier)
-        
         return collectionView;
         
     }()
@@ -32,13 +25,11 @@ class CollectionViewTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        contentView.backgroundColor = .systemPurple;
+        contentView.backgroundColor = .systemBackground;
         contentView.addSubview(collectionView);
-        
         
         collectionView.delegate = self;
         collectionView.dataSource = self;
-        
     }
     
     required init?(coder: NSCoder) {
@@ -47,7 +38,6 @@ class CollectionViewTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews();
-        
         collectionView.frame = contentView.bounds;
         
     }
@@ -58,7 +48,6 @@ class CollectionViewTableViewCell: UITableViewCell {
         DispatchQueue.main.async {
             [weak self] in
             self?.collectionView.reloadData()
-            
         }
     }
 }
@@ -71,15 +60,11 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
             return UICollectionViewCell();
         }
         
-        //cell.backgroundColor = .green
         guard let model = titles[indexPath.row].poster_path else {
             
             return UICollectionViewCell();
-            
         }
-        
         cell.configure(with: model)
-        
         return cell
     }
     
