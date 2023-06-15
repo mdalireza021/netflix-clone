98e1c79d9be069b9f3487855475b0f2c625b69c7
Muhammad Ali <muhammad.ali@brainstation-23.com>
2023-06-15 17:57:17 +0600

    implement search method

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -137,4 +137,26 @@ class APICaller {
         }
         task.resume();
     }
+    
+    /// search
+    func search(with query: String, completion:  @escaping ( Result<[Title], Error>) -> Void ) {
+        
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {return}
+        
+        guard let url = URL(string: "\(Constants.baseURL)/3/search/movie?api_key=\(Constants.API_KEY)&query=\(query)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume();
+    }
 }
