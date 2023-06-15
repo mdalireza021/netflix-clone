87b9f840158a071a49362d02d8ecdfb957a63832
Muhammad Ali <muhammad.ali@brainstation-23.com>
2023-06-15 15:57:45 +0600

    implement get discover movies

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -38,7 +38,6 @@ class APICaller {
             }
         }
         task.resume();
-        
     }
     
     ///Tranding Tv s
@@ -46,7 +45,6 @@ class APICaller {
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/tv/day?api_key=\(Constants.API_KEY)") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
-            
             guard let data = data, error == nil else {
                 return
             }
@@ -57,10 +55,8 @@ class APICaller {
             } catch {
                 print(error.localizedDescription)
             }
-            
         }
         task.resume();
-        
     }
     
     
@@ -69,7 +65,6 @@ class APICaller {
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/popular?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
-            
             guard let data = data, error == nil else {
                 return
             }
@@ -80,46 +75,36 @@ class APICaller {
             } catch {
                 print(error.localizedDescription)
             }
-            
         }
         task.resume();
-        
     }
     
     
     /// Upcoming movies
-    
     func getUpcomingMovies( completion:  @escaping ( Result<[Title], Error>) -> Void ) {
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/upcoming?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
-        
+
         let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
-            
             guard let data = data, error == nil else {
                 return
             }
             do {
                 let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 completion(.success(results.results))
-                
             } catch {
                 print(error.localizedDescription)
             }
-            
         }
         task.resume();
-        
     }
     
     
     
     /// Top rated movies
-    
     func getTopRatedMovies(completion:  @escaping ( Result<[Title], Error>) -> Void ) {
-        
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/top_rated?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
-            
             guard let data = data, error == nil else {
                 return
             }
@@ -130,10 +115,26 @@ class APICaller {
             } catch {
                 print(error.localizedDescription)
             }
-            
         }
         task.resume();
-        
     }
-  
+    
+    /// get  DiscoverMovies
+    func getDiscoverMovies(completion:  @escaping ( Result<[Title], Error>) -> Void ) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/discover/movie?api_key=\(Constants.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url:url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume();
+    }
 }
