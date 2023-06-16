54c94d26a6e0d2ae4d3bca1a9f6d8cf69c824ea9
md ali reza <muhammd.ali@brainstation-23.com>
2023-06-16 18:02:34 +0600

    initial commit

---
+++ Netflix Clone/Controllers/General/TitlePreviewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by BS1096 on 16/6/23.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+    
+}
