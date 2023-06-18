ec0c2c3008f27f84c72e5d8c5dac182812c7e26a
md ali reza <muhammd.ali@brainstation-23.com>
2023-06-18 14:39:45 +0600

    update collectionView

--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
+protocol CollectionViewTableViewCellDelegate: AnyObject {
+    func collectionViewTableViewCellDidTapCell(_cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel)
+}
+
 class CollectionViewTableViewCell: UITableViewCell {
     static let identifier = "CollectionViewTableViewCell";
-    private var titles: [Title] = [Title] ();
     
+    weak var delegate: CollectionViewTableViewCellDelegate?
+    private var titles: [Title] = [Title] ();
     private let collectionView: UICollectionView = {
-        
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: 140, height: 200)
         layout.scrollDirection  = .horizontal
@@ -39,7 +43,6 @@ class CollectionViewTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews();
         collectionView.frame = contentView.bounds;
-        
     }
     
     public func configure(with titles: [Title]) {
@@ -53,7 +56,6 @@ class CollectionViewTableViewCell: UITableViewCell {
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
@@ -61,14 +63,38 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         }
         
         guard let model = titles[indexPath.row].poster_path else {
-            
             return UICollectionViewCell();
         }
+        
         cell.configure(with: model)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return titles.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        let title = titles[indexPath.row]
+        guard let titleName = title.original_name ?? title.original_title else { return }
+        
+        APICaller.shared.getMovies(with: titleName + " trailer") { [weak self] result in switch result {
+            
+        case .success(let videoElement):
+            let title = self?.titles[indexPath.row]
+            guard let titleOverview = title?.overview else {return}
+            guard let strongSelf = self else { return }
+            
+            let viewModel = TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: titleOverview)
+            
+            self?.delegate?
+                .collectionViewTableViewCellDidTapCell(_cell: strongSelf, viewModel: viewModel)
+            
+            print(videoElement.id)
+        case .failure(let error):
+            print(error.localizedDescription)
+            }
+        }
     }
 }
