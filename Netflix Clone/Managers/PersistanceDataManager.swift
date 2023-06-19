3e6df0b8d6ccb055555197b4d50e9ae84aa3c063
md ali reza <muhammd.ali@brainstation-23.com>
2023-06-19 11:57:27 +0600

    initial commit

---
+++ Netflix Clone/Managers/PersistanceDataManager.swift
@@ -0,0 +1,17 @@
+//
+//  PersistanceDataManager.swift
+//  Netflix Clone
+//
+//  Created by BS1096 on 18/6/23.
+//
+
+import Foundation
+import CoreData
+
+class DownloadManager {
+    static let shared = DownloadManager()
+    
+    func downloadItem(with model: Title) {
+        
+    }
+}
