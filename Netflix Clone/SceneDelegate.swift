d3fcf4ea945f1f177da32bbaed8db6e00f390b07
md ali reza <muhammd.ali@brainstation-23.com>
2023-06-19 12:00:24 +0600

    initial commit

--- Netflix Clone/SceneDelegate.swift
+++ Netflix Clone/SceneDelegate.swift
@@ -4,24 +4,17 @@
 //
 //  Created by BS1096 on 7/6/23.
 //
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds);
-        window?.windowScene = windowScene;
-        window?.rootViewController = MainTabBarViewController();
-        window?.makeKeyAndVisible();
-        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = MainTabBarViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -51,7 +44,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
